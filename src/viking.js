07258aeb08c16829858b32b8247e2e5ec2db83eb
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-07-14 18:07:58 +0200

    lab class 05

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }  
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(thedamage){
+
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor (name, health, strength){
+        super (health, strength);
+    }
+}
 
 // Saxon
 class Saxon {}
